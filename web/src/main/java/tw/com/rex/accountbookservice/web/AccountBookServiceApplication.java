de123b2417c3959dadd5bdb883b977e3397903a6
Rex Yu <yu.chenhang@gmail.com>
2019-03-26 14:00:34 +0800

    設定UTF-8編碼及可打包成tomcat使用的war

--- web/src/main/java/tw/com/rex/accountbookservice/web/AccountBookServiceApplication.java
+++ web/src/main/java/tw/com/rex/accountbookservice/web/AccountBookServiceApplication.java
@@ -3,13 +3,22 @@ package tw.com.rex.accountbookservice.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"tw.com.rex.accountbookservice.model.dao"})
-public class AccountBookServiceApplication {
+@EnableJpaRepositories(basePackages = {"tw.com.rex.accountbookservcie.repository"})
+@EntityScan(basePackages = {"tw.com.rex.accountbookservice.model"})
+public class AccountBookServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountBookServiceApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AccountBookServiceApplication.class);
     }
 
 }
