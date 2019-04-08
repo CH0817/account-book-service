7baa98fb71bce2339cd80b72113524d77fe078c4
Rex Yu <yu.chenhang@gmail.com>
2019-04-08 13:55:11 +0800

    move package

--- web/src/main/java/tw/com/rex/accountbookservice/AccountBookServiceApplication.java
+++ web/src/main/java/tw/com/rex/accountbookservice/AccountBookServiceApplication.java
@@ -1,15 +1,11 @@
-package tw.com.rex.accountbookservice.web;
+package tw.com.rex.accountbookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"tw.com.rex.accountbookservcie.repository"})
-@EntityScan(basePackages = {"tw.com.rex.accountbookservice.model"})
 public class AccountBookServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
