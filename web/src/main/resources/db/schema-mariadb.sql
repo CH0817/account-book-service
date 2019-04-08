10bcc613ce279d97f2ba5c7789b9b0081fce4764
Rex Yu <yu.chenhang@gmail.com>
2019-04-08 14:24:35 +0800

    modify

--- web/src/main/resources/db/schema-mariadb.sql
+++ web/src/main/resources/db/schema-mariadb.sql
@@ -7,71 +7,72 @@ DROP TABLE IF EXISTS `currency`;
 
 CREATE TABLE IF NOT EXISTS account_type
 (
-  id          INT         NOT NULL AUTO_INCREMENT,
-  name        VARCHAR(10) NOT NULL UNIQUE,
-  create_date DATE        NOT NULL,
-  update_date DATE,
-  PRIMARY KEY (id)
+    id          BIGINT         NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(10) NOT NULL UNIQUE,
+    create_date DATE        NOT NULL,
+    update_date DATE,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS currency
 (
-  id          INT         NOT NULL AUTO_INCREMENT,
-  name        VARCHAR(10) NOT NULL UNIQUE,
-  create_date DATE        NOT NULL,
-  update_date DATE,
-  PRIMARY KEY (id)
+    id          BIGINT         NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(10) NOT NULL UNIQUE,
+    create_date DATE        NOT NULL,
+    update_date DATE,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS account
 (
-  id               INT         NOT NULL AUTO_INCREMENT,
-  name             VARCHAR(10) NOT NULL UNIQUE,
-  account_type_id  INT         NOT NULL,
-  currency_id      INT         NOT NULL,
-  init_money       DECIMAL(9, 2),
-  current_money    DECIMAL(9, 2),
-  closing_date     DATE,
-  payment_due_date DATE,
-  note             VARCHAR(150),
-  create_date      DATE        NOT NULL,
-  update_date      DATE,
-  PRIMARY KEY (id),
-  FOREIGN KEY (account_type_id) REFERENCES account_type (id),
-  FOREIGN KEY (currency_id) REFERENCES currency (id)
+    id               BIGINT         NOT NULL AUTO_INCREMENT,
+    name             VARCHAR(10) NOT NULL UNIQUE,
+    account_type_id  BIGINT         NOT NULL,
+    currency_id      BIGINT         NOT NULL,
+    init_money       DECIMAL(9, 2),
+    current_money    DECIMAL(9, 2),
+    closing_date     DATE,
+    payment_due_date DATE,
+    note             VARCHAR(150),
+    create_date      DATE        NOT NULL,
+    update_date      DATE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (account_type_id) REFERENCES account_type (id),
+    FOREIGN KEY (currency_id) REFERENCES currency (id)
 );
 
 CREATE TABLE IF NOT EXISTS category
 (
-  id            INT         NOT NULL AUTO_INCREMENT,
-  name          VARCHAR(10) NOT NULL UNIQUE,
-  category_type INT         NOT NULL,
-  create_date   DATE        NOT NULL,
-  update_date   DATE,
-  PRIMARY KEY (id)
+    id            BIGINT         NOT NULL AUTO_INCREMENT,
+    name          VARCHAR(10) NOT NULL UNIQUE,
+    category_type BIGINT         NOT NULL,
+    create_date   DATE        NOT NULL,
+    update_date   DATE,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS item
 (
-  id          INT         NOT NULL AUTO_INCREMENT,
-  name        VARCHAR(10) NOT NULL UNIQUE,
-  category_id INT         NOT NULL,
-  create_date DATE        NOT NULL,
-  update_date DATE,
-  PRIMARY KEY (id),
-  FOREIGN KEY (category_id) REFERENCES category (id)
+    id          BIGINT         NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(10) NOT NULL UNIQUE,
+    category_id BIGINT         NOT NULL,
+    create_date DATE        NOT NULL,
+    update_date DATE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (category_id) REFERENCES category (id)
 );
 
 CREATE TABLE IF NOT EXISTS trade
 (
-  id            INT  NOT NULL AUTO_INCREMENT,
-  account_id    INT  NOT NULL,
-  item_id       INT  NOT NULL,
-  transact_date DATE NOT NULL,
-  note          VARCHAR(150),
-  create_date   DATE NOT NULL,
-  update_date   DATE,
-  PRIMARY KEY (id),
-  FOREIGN KEY (account_id) REFERENCES account (id),
-  FOREIGN KEY (item_id) REFERENCES item (id)
+    id            BIGINT     NOT NULL AUTO_INCREMENT,
+    account_id    BIGINT     NOT NULL,
+    item_id       BIGINT     NOT NULL,
+    transact_date DATE    NOT NULL,
+    cost          DECIMAL(9, 2) NOT NULL,
+    note          VARCHAR(150),
+    create_date   DATE    NOT NULL,
+    update_date   DATE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (account_id) REFERENCES account (id),
+    FOREIGN KEY (item_id) REFERENCES item (id)
 );
