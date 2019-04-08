7a56c5ae16030b7bf3f3ec17de345a10318d82a1
Rex Yu <yu.chenhang@gmail.com>
2019-04-08 18:00:01 +0800

    add income story

---
+++ web/src/test/java/tw/com/rex/accountbookservice/step/IncomeStep.java
@@ -0,0 +1,39 @@
+package tw.com.rex.accountbookservice.step;
+
+import org.jbehave.core.annotations.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IncomeStep {
+
+    @Given("I am in the income page")
+    public void toPage() {
+        // TODO
+    }
+
+    @When("I input all necessary data")
+    public void inputAllNecessaryData() {
+        // TODO
+    }
+
+    @When("click save button")
+    public void clickSaveButton() {
+        // TODO
+    }
+
+    @Then("show me a message")
+    public void showSuccessMessage() {
+        // TODO
+    }
+
+    @Then("display saved income on the page")
+    public void displaySavedIncome() {
+        // TODO
+    }
+
+    @When("I not input all necessary data")
+    public void notInputAllData() {
+        // TODO
+    }
+
+}
