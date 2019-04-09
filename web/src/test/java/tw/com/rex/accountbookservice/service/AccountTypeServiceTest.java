d1605752ff7693247e3099b6238b0a7ed4ed081f
Rex Yu <yu.chenhang@gmail.com>
2019-04-09 10:26:22 +0800

    test service without database.

--- web/src/test/java/tw/com/rex/accountbookservice/service/AccountTypeServiceTest.java
+++ web/src/test/java/tw/com/rex/accountbookservice/service/AccountTypeServiceTest.java
@@ -2,31 +2,37 @@ package tw.com.rex.accountbookservice.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import tw.com.rex.accountbookservice.AccountBookServiceApplication;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import tw.com.rex.accountbookservice.model.dao.AccountTypeDAO;
 import tw.com.rex.accountbookservice.model.vo.AccountTypeVO;
+import tw.com.rex.accountbookservice.repository.AccountTypeRepository;
 import tw.com.rex.accountbookservice.service.impl.AccountTypeServiceImpl;
 
-import javax.transaction.Transactional;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
-import static org.junit.Assert.assertNotNull;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = AccountBookServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@RunWith(MockitoJUnitRunner.class)
 public class AccountTypeServiceTest {
 
-    @Autowired
+    @InjectMocks
     private AccountTypeServiceImpl service;
+    @Mock
+    private AccountTypeRepository repository;
 
     @Test
-    // @Sql("/db/schema-mariadb.sql")
-    @Transactional
     public void save() throws Exception {
-        AccountTypeVO vo = service.save(new AccountTypeDAO("測試"));
-        assertNotNull(vo.getId());
+        // given
+        AccountTypeDAO entity = new AccountTypeDAO();
+        AccountTypeDAO dao = new AccountTypeDAO();
+        dao.setId(1L);
+        // when
+        when(repository.save(entity)).thenReturn(dao);
+        AccountTypeVO vo = service.save(entity);
+        // then
+        verify(repository, atLeastOnce()).save(entity);
+        assertEquals(1L, vo.getId().longValue());
     }
 
 }
