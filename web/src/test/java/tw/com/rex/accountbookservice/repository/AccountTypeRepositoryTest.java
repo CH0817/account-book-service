9b013c5567066f51c21cc5378f0fc4f491e9edf7
Rex Yu <yu.chenhang@gmail.com>
2019-04-15 11:23:10 +0800

    refactor repository tests

--- web/src/test/java/tw/com/rex/accountbookservice/repository/AccountTypeRepositoryTest.java
+++ web/src/test/java/tw/com/rex/accountbookservice/repository/AccountTypeRepositoryTest.java
@@ -1,25 +1,18 @@
 package tw.com.rex.accountbookservice.repository;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Example;
-import org.springframework.test.context.junit4.SpringRunner;
 import tw.com.rex.accountbookservice.model.dao.AccountTypeDAO;
+import tw.com.rex.accountbookservice.repository.base.BaseRepositoryTest;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
-import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
-@RunWith(SpringRunner.class)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = NONE)
-public class AccountTypeRepositoryTest {
+public class AccountTypeRepositoryTest extends BaseRepositoryTest {
 
     @Autowired
     private AccountTypeRepository repository;
