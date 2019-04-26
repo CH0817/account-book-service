40eee0db20ee7935798687fbb1db2d252683c55d
Rex Yu <yu.chenhang@gmail.com>
2019-04-26 15:09:40 +0800

    add swagger

---
+++ web/src/main/java/tw/com/rex/accountbookservice/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package tw.com.rex.accountbookservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)//
+                .apiInfo(apiInfo())//
+                .select()//
+                .apis(RequestHandlerSelectors.basePackage("tw.com.rex.accountbookservice"))//
+                .paths(PathSelectors.any())//
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()//
+                .title("Account book RESTful API")//
+                .description("...")//
+                .contact(new Contact("Rex Yu", "https://", "yu.chenhang@gmail.com"))//
+                .version("1.0")//
+                .build();
+    }
+
+}
