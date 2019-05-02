08146b37a1b03bf063e77c33982ac2669de65f74
Rex Yu <yu.chenhang@gmail.com>
2019-05-02 14:08:15 +0800

    AccountTypeRepositoryTest is OK

--- web/src/main/resources/db/data/test/data-account_type.sql
+++ web/src/main/resources/db/data/test/data-account_type.sql
@@ -1,3 +1,3 @@
-INSERT INTO account_type (id, name, create_date) VALUES (66, '銀行', NOW());
-INSERT INTO account_type (id, name, create_date) VALUES (77, '信用卡', NOW());
-INSERT INTO account_type (id, name, create_date) VALUES (88, '現金', NOW());
+INSERT INTO account_type (name, create_date) VALUES ('銀行', NOW());
+INSERT INTO account_type (name, create_date) VALUES ('信用卡', NOW());
+INSERT INTO account_type (name, create_date) VALUES ('現金', NOW());
--- web/src/test/java/tw/com/rex/accountbookservice/repository/AccountTypeRepositoryTest.java
+++ web/src/test/java/tw/com/rex/accountbookservice/repository/AccountTypeRepositoryTest.java
@@ -2,8 +2,13 @@ package tw.com.rex.accountbookservice.repository;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+import tw.com.rex.accountbookservice.dao.AccountTypeDAO;
 import tw.com.rex.accountbookservice.repository.base.BaseRepositoryTest;
 
+import static org.junit.Assert.assertNotNull;
+
+@Sql({"/db/data/test/data-account_type.sql"})
 public class AccountTypeRepositoryTest extends BaseRepositoryTest {
 
     @Autowired
@@ -11,7 +16,8 @@ public class AccountTypeRepositoryTest extends BaseRepositoryTest {
 
     @Test
     public void findByName() {
-
+        AccountTypeDAO dao = repository.findByName("銀行");
+        assertNotNull(dao);
     }
 
 }
