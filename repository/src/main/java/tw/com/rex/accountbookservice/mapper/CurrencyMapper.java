b2b801e09ef0c324421384be38e6723102fb0a79
Rex Yu <yu.chenhang@gmail.com>
2019-05-14 15:21:17 +0800

    create CurrencyMapperTest and add selectAll()

--- repository/src/main/java/tw/com/rex/accountbookservice/mapper/CurrencyMapper.java
+++ repository/src/main/java/tw/com/rex/accountbookservice/mapper/CurrencyMapper.java
@@ -1,7 +1,11 @@
 package tw.com.rex.accountbookservice.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import tw.com.rex.accountbookservice.model.dao.Currency;
 
+import java.util.List;
+
+@Mapper
 public interface CurrencyMapper {
 
     int deleteByPrimaryKey(String id);
@@ -12,4 +16,5 @@ public interface CurrencyMapper {
 
     int updateByPrimaryKeySelective(Currency record);
 
+    List<Currency> selectAll();
 }
